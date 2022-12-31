5198a9e92ab461f572edddb62eb956240c5dd554
Dennis Martin <dennis.martin.was.here@gmail.com>
2022-12-31 15:13:13 -0500

    update maxheight

--- assets/js/energy.js
+++ assets/js/energy.js
@@ -84,7 +84,7 @@ function populateDD()
                     parameter = "year";
                     getCSVData(parameter, filter)
                 })
-                // .style("max-height", getHeight(svgcanvas));       
+               
 
     // When user selects a year from the countrySelector dropdown, it will set the paramter and filter variables. These are saved outside of the function itself in case a window
     // resize event is called, it can redisplay the same information without the user having to click another option. When user clicks it will also disable the tutorial text.            
